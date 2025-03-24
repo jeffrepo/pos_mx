f0d98aa017c1cd19bdc4542bc9ac1cc78f19af42
jeffrepo <jeffer.leo93@gmail.com>
2025-03-24 11:55:36 -0600

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -19,8 +19,8 @@
     'data': [
         'data/paperformat_ticket_stock.xml',
         'report/reporte_existencias.xml',
-        'report/reporte_entrega_valores.xml',
-        'reporte_entrega_valores_wizard.xml',
+        'report/report/reporte_entrega_valores.xml',
+        'wizard/reporte_entrega_valores_wizard.xml',
         'views/report.xml',
         'wizard/reporte_existencias_wizard.xml',
     ],
